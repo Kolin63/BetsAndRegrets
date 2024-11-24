d6a4bf32d843beb4fb562d93649ffa5a46a0bdd7
Kolin <theverylastgoodname@gmail.com>
2024-11-23 19:59:52 -0500

    comittt!!!

---
+++ dialouge_manager.gd
@@ -0,0 +1,6 @@
+extends Node
+
+func dia(index, Bubble):
+	if (index == 0):
+		Bubble.say("You've lived a bad life, and now you're in the afterlife. You want a second chance, so you made a deal with the devil. You need to get 10 million dollars. You can get it by working for hundreds of years, or you could gamble it all for a chance to win BIG. Luckily, you have $5.32 from your wallet when you died. That's more than enough to gamble! And besides, most gamblers quit before they win big, so naturally, you won't quit, right? But be careful when you're gambling! If you lose too much, you'll be the devil's indentured servant...", )
+		
--- plinko.gd
+++ plinko.gd
@@ -2,6 +2,7 @@ extends Node2D
 @onready var Peg = $Peg
 @onready var Bucket = $Bucket
 @onready var Ball = $Ball
+@onready var Bubble = $Bubble
 
 var money
 var balls 
@@ -11,11 +12,14 @@ var soul = 4
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$DialougeManager.dia(0, Bubble)
 	money = 5.32
 	balls = 1
 	set_soul(4)
 	set_balls(day)
 	generate(18)
+	
+	$Bubble.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -24,7 +28,7 @@ func _process(delta: float) -> void:
 	if (Input.is_action_just_pressed("click") && balls > 0):
 		var ball = Ball.duplicate()
 		ball.position = get_viewport().get_mouse_position()
-		ball.position.y = 25
+		#ball.position.y = 25
 		ball.gravity_scale = 1
 		add_child(ball)
 		set_balls(balls - 1)
@@ -55,6 +59,11 @@ func get_money():
 func set_money(x):
 	money = x
 	$Money.text = "$" + str(money)
+	if (money == 0):
+		set_soul(soul - 1)
+		day = 1
+		set_balls(day)
+		set_money(5.32)
 
 
 func set_balls(x):
@@ -151,3 +160,10 @@ func generate(base_pegs):
 				var dist_from_center = abs(pegs_in_row / 2 - j)
 				bucket.set_multiplier(0.25 * dist_from_center * dist_from_center / 2)
 				bucket.set_multiplier((0.25 * dist_from_center * dist_from_center) / 2)
+
+
+func say(text, audio):
+	$Bubble.visible = true
+	$Bubble/Label.text = text
+	$AudioStreamPlayer2D.stream = audio
+	$AudioStreamPlayer2D.play()
